2d60de87d0b572a8577e1692f876f314ecbc91d9
Nithinbr95 <nithin.xworkz@gmail.com>
2024-06-24 13:05:01 +0530

    Friday task

---
+++ Student.java
@@ -0,0 +1,21 @@
+public class Student{
+public static void main(String[] args){
+System.out.println("Displaying a Student Details");
+short marks = 60;
+int id = 4203;
+float percentage = 60;
+double average = 70;
+boolean pass = true;
+byte age= 31;
+long contactNumber = 9675843587L;
+
+System.out.println("id is:"+id);
+System.out.println("marks is:"+marks );
+System.out.println("percentage is:"+percentage);
+System.out.println(" pass:"+pass );
+System.out.println("average :"+average);
+System.out.println("contactNumber is:"+contactNumber);
+System.out.println("age are:"+age);
+System.out.println("The Classname is Student");
+}
+}
---
+++ Subject.java
@@ -0,0 +1,8 @@
+public class Subject{
+
+static String java = "James Gosling";
+static String c = "Dennis Ritchie";
+static String python = "Guido van Rossum";
+static String html = "Tim Berners-Lee";
+static String javascript = "Brendan Eich";
+}
